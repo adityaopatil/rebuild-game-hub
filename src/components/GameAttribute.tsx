5ec51e98c14411b28fdc98d975b5c24803b74b5d
unknown <patiladi3119@gmail.com>
2024-01-28 18:48:15 +0530

    Added game attribute

---
+++ src/components/GameAttribute.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Game } from "../entities/Game";
+import CriticScore from "./CriticScore";
+import { Genre } from "../entities/Genre";
+
+interface Props {
+  game: Game;
+}
+
+const GameAttribute = ({ game }: Props) => {
+  const headerStyle = "font-bold text-xl inline-block mr-2 text-gray-500";
+  const listStyle = "hover:text-gray-200";
+
+  return (
+    <div className="my-4 grid grid-cols-1 gap-x-4 gap-y-10 sm:grid-cols-2">
+      <div>
+        <h1 className={headerStyle}>Platforms</h1>
+        <ul>
+          {game.parent_platforms.map((p) => (
+            <li className={listStyle} key={p.platform.id}>
+              {p.platform.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h1 className={headerStyle}>Metascore</h1>
+        <div className="mt-1 flex justify-between">
+          <CriticScore score={game.metacritic} />
+        </div>
+      </div>
+      <div>
+        <h1 className={headerStyle}>Genres</h1>
+        <ul>
+          {game.genres.map((g) => (
+            <li className={listStyle} key={g.id}>
+              {g.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h1 className={headerStyle}>Publishers</h1>
+        <ul>
+          {game.publishers.map((pub) => (
+            <li className={listStyle} key={pub.id}>
+              {pub.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default GameAttribute;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,10 +2,13 @@ import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { useState } from "react";
 import ExpandableText from "../components/ExpandableText";
+import GameAttribute from "../components/GameAttribute";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
+
+  console.log(game);
 
   if (isLoading)
     return (
@@ -21,6 +24,7 @@ const GameDetailPage = () => {
     <>
       <h2 className="text-5xl font-bold my-4">{game.name}</h2>
       <ExpandableText children={game.description_raw} />
+      <GameAttribute game={game} />
     </>
   );
 };
