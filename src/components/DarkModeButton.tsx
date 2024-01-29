0b324ef1c7cb56909d545382bb8dbeb2dd3c5d70
unknown <patiladi3119@gmail.com>
2024-01-29 15:29:55 +0530

    Updated the dark mode button as cursor

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -30,13 +30,13 @@ const DarkModeButton = () => {
             theme === "dark"
               ? "bg-green-600 ring-black/20"
               : "bg-green-950/10 ring-green-950/5"
-          }`}
+          } cursor-pointer`}
           onClick={handleToggle}
         >
           <div
             className={`h-4 w-4 rounded-full bg-white shadow-sm ring-1 ring-slate-700/10 transition duration-200 ease-in-out ${
               theme === "light" ? "translate-x-4" : ""
-            }`}
+            } cursor-pointer`}
           ></div>
         </div>
         <span className="ml-2 whitespace-nowrap">Dark Mode</span>
