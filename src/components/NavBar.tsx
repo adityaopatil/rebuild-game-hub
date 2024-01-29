23928bb7d705375b8df94fd1204c40d4d456ee05
unknown <patiladi3119@gmail.com>
2024-01-29 10:58:57 +0530

    Fixed nav button and screen resize

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,12 +7,10 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="flex flex-row items-center justify-between pr-2 pt-3">
-      <Link to="/" className="mr-1 ml-2">
-        <img
-          src={logo}
-          alt="game hub logo"
-          className="w-[70px] h-[70px] object-cover"
-        />
+      <Link to="/">
+        <div className="w-[70px] h-[70px]">
+          <img src={logo} alt="game hub logo" className=" object-cover" />
+        </div>
       </Link>
       <SearchInput />
       <DarkModeButton />
